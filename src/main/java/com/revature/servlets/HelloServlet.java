c0ff55cd2412e511c9918dd587f752d938a68c69
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-08-13 08:45:19 -0400

    init with servlet project

---
+++ src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	protected void doGet(HttpServletRequest rq, HttpServletResponse rs) throws IOException, ServletException{
+		try(PrintWriter pw = rs.getWriter()){
+			pw.write("Hello World from EC2!");
+		}
+	}
+	
+}
